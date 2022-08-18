fd248080507c08048319397ca9bb6e5b5402949c
ManasiAdhaoo <99181309+ManasiAdhaoo@users.noreply.github.com>
2022-08-18 06:45:33 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import Optimizer
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def home():
     return "<h1> Hello World </h1>"
 
 @app.route('/app/') 
+@cross_origin()
 def result():
     tickers = request.args.get('tickers')
     #tickers = "GOOGL TSLA"
     minr, maxr = Optimizer.optimize(tickers)
     data = {"minr": minr.to_json(), "maxr": maxr.to_json()}
-    print(minr.to_json())
-    print(maxr.to_json())
-    
-    print(data)
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
-    
+    app.run(debug=True)
